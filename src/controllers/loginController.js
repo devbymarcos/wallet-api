b1a977a3091ac8bcf92f82cec5df53aa1d5a5aa0
devbymarcos <marcoslopes.dev@gmail.com>
2021-10-27 19:09:48 -0300

    criando controle de recupração de senha

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,6 +1,7 @@
 import { User } from '../models/User.js';
 import bcryptjs from 'bcryptjs';
 import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
 import jwt from 'jsonwebtoken';
 const { sign, verify } = jwt;
 
@@ -40,3 +41,57 @@ export const logout = (req,res)=>{
   res.redirect('/login');
 }
 
+export const forgetAction = async(req, res)=>{
+
+    const user = await User.findOne({
+        where:{
+            email:req.body.email
+        }
+    })
+    if(!user){
+        res.json({message:'Usuário não encontrado',type:'error'});
+        return;
+    }
+
+    const token = sign(
+        {id:user.id,email:user.email},
+        String(process.env.SECRET_KEY_JWT)
+    )
+    
+    
+    const link = `${process.env.URL_BASE}/altera-senha/${token}`;
+    
+     let emailTemplate = `<h2>Perdeu sua senha ${user.fullName} ?</h2>"
+    <p>Você está recebendo este e-mail pois foi solicitado a recuperação de senha no site ${process.env.URL_BASE} </p>
+    <p><a title='Recuperar Senha' href='${link}'> CLIQUE AQUI PARA CRIAR UMA NOVA SENHA</a></p>
+    <p><b>IMPORTANTE:</b> Se não foi você que solicitou ignore o e-mail. Seus dados permanecem seguros.</p>`;
+
+    
+
+    //configurando o tranporte
+    let transport = nodemailer.createTransport({
+        host:'devbymarcos.com',
+        port:'456',
+        auth:{
+            user:'marcos@devbymarcos.com',
+            pass:'w91xQLnUk88A'
+        }
+    });
+    // configura a menssagem
+    let message = {
+        from:'',
+        to:'',
+        subject:'',
+        html:'',
+        text:''
+    }
+    //envia a mensagem
+    // let info = await transport.sendMail(message);
+    // console.log(info)
+
+    res.json({action:'processando'});
+}
+
+export const viewRecoveryPass = (req,res)=>{
+    console.log(req.params)
+}
--- src/routes/router.js
+++ src/routes/router.js
@@ -20,6 +20,8 @@ router.get('/',UserController.viewRegister);
 router.get('/login',LoginController.viewLogin);
 router.post('/login-validation',LoginController.loginAuth);
 router.get('/logoff',LoginController.logout);
+router.post('/forget-action',LoginController.forgetAction);
+router.get('/altera-senha/:token',LoginController.viewRecoveryPass)
 //user
 router.get('/perfil',Auth.privateRouter,UserController.viewPerfil);
 
