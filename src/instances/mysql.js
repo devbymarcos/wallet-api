831b3e81f5b0194ea18e1b5b96e5f8b780da2674
Marcos Lopes <marcos.41code@gmail.com>
2021-11-13 19:19:13 -0300

    erro no isntance

--- src/instances/mysql.js
+++ src/instances/mysql.js
@@ -1,14 +1,14 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 dotenv.config();
 
 export const sequelize = new Sequelize(
-  process.env.MYSQL_DB,
-  process.env.MYSQL_USER,
-  process.env.MYSQL_PASSWORD,
-  {
-    dialect:'mysql',
-    host:process.env.MYSLQ_HOST,
-    port:parseInt(process.env.MYSQL_PORT)
-  }
+    process.env.MYSQL_DB,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD,
+    {
+        dialect: "mysql",
+        host: process.env.MYSQL_HOST,
+        port: parseInt(process.env.MYSQL_PORT),
+    }
 );
