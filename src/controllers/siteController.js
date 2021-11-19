4970809411f72dc0822a96b83794e2596d633103
devbymarcos <marcoslopes.dev@gmail.com>
2021-11-18 22:15:59 -0300

    reconfigurado rotas e criado diretório site

---
+++ src/controllers/siteController.js
@@ -0,0 +1,7 @@
+export const viewRegister = (req, res) => {
+    res.render("pages/widgets/site/user-create");
+};
+
+export const viewTerms = (req, res) => {
+    res.render("pages/widgets/site/terms");
+};
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,33 +1,28 @@
-import { User } from '../models/User.js';
+import { User } from "../models/User.js";
 
-
-export const viewRegister = (req,res)=>{
-
-    res.render('pages/widgets/user/user-create')
-
-}
-export const viewPerfil = async(req,res)=>{
+export const viewRegister = (req, res) => {
+    res.render("pages/widgets/user/user-create");
+};
+export const viewPerfil = async (req, res) => {
     const userSession = req.session.user;
-    const findUser =  await User.findByPk(userSession);
-   
-    const cover =  (findUser.photo === 'default' ? false  : true);
+    const findUser = await User.findByPk(userSession);
 
-    const select = (type,value)=>{
-        return(type == value ? 'selected' : '');
-    }
-  
-    const selMale = select(findUser.genre,'male');
-    const selFemale = select(findUser.genre,'female');
-    res.render('pages/widgets/user/user-perfil',{
+    const cover = findUser.photo === "default" ? false : true;
+
+    const select = (type, value) => {
+        return type == value ? "selected" : "";
+    };
+
+    const selMale = select(findUser.genre, "male");
+    const selFemale = select(findUser.genre, "female");
+    res.render("pages/widgets/user/user-perfil", {
         findUser,
         cover,
         selMale,
-        selFemale
-    })
+        selFemale,
+    });
+};
 
-}
-
-export const save = async(req,res)=>{
-
-    res.json({retorno:'deutudo certo'});
-}
+export const save = async (req, res) => {
+    res.json({ retorno: "deutudo certo" });
+};
