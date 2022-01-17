9632b5126dd6125de6e0addc5a0f5edc74cf9e34
devbymarcos <marcoslopes.dev@gmail.com>
2022-01-16 22:41:20 -0300

    feature : inserido dados na tela de investimento

--- src/controllers/investmentController.js
+++ src/controllers/investmentController.js
@@ -1,7 +1,51 @@
+import { Invoice } from "../models/Invoice.js";
 
+export const create = async (req, res) => {
+    const invest = await Invoice.findAll({
+        where: {
+            user_id: req.session.user,
+            wallet_id: 26,
+        },
+        order: [["due_at", "DESC"]],
+    });
 
+    let dataInvest = [];
+    invest.forEach((item) => {
+        //formata data
+        let dateArr = item.due_at.split("-");
+        let [year, month, day] = dateArr.map(Number);
 
-export const create = (req,res)=>{
-   res.render("pages/widgets/investment/investment")
+        let date = new Date(year, month - 1, day);
+        let dateFormat =
+            date.getDate() +
+            "/" +
+            (date.getMonth() + 1) +
+            "/" +
+            date.getFullYear();
 
-}
+        let price = item.price.toLocaleString("pt-br", {
+            style: "currency",
+            currency: "BRL",
+        });
+        //formata status
+        let statusPay = "";
+        if (item.pay === "paid") {
+            statusPay = true;
+        } else {
+            statusPay = false;
+        }
+
+        //cria novo objto com dados formatado
+        dataInvest.push({
+            id: item.id,
+            date: dateFormat,
+            description: item.description,
+            status: statusPay,
+            value: price,
+            type: item.type,
+        });
+    });
+    res.render("pages/widgets/investment/investment", {
+        dataInvest,
+    });
+};
