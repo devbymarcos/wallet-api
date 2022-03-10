0c0c277bd31f4fe33f336d841d953ad2cec94443
devbymarcos <marcoslopes.dev@gmail.com>
2022-03-10 00:32:33 -0300

    CREATE: update para typepost criado

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,127 +1,130 @@
-import { Category } from '../models/Category.js';
+import { Category } from "../models/Category.js";
 
-export const category = async(req,res)=>{
+export const category = async (req, res) => {
+    let categories = await Category.findAll({
+        where: {
+            user_id: req.session.user,
+        },
+    });
 
-    let categories =  await Category.findAll({
-      where:{
-        user_id:req.session.user
-      }
-    })
-
-    let typeInvoice =()=>{
+    let typeInvoice = () => {
         let arr = [];
-        categories.forEach((item)=>{
-          if(item.type === 'expense'){
-            arr.push(
-              {id:item.id,user_id:item.user_id,name:item.name,description:item.description,type:'Despesas'}
-              )
-          }else{
-            arr.push(
-              {id:item.id,user_id:item.user_id,name:item.name,description:item.description,type:'Receita'}
-            )
-          }
-            
-        })
-        
+        categories.forEach((item) => {
+            if (item.type === "expense") {
+                arr.push({
+                    id: item.id,
+                    user_id: item.user_id,
+                    name: item.name,
+                    description: item.description,
+                    type: "Despesas",
+                });
+            } else {
+                arr.push({
+                    id: item.id,
+                    user_id: item.user_id,
+                    name: item.name,
+                    description: item.description,
+                    type: "Receita",
+                });
+            }
+        });
+
         return arr;
-    } 
-   let activeMessage = '';
-   if(req.query.category && req.query.category === 'not'){  
-        activeMessage = true
-   }
-   const userName  = req.session.fullName;
-    res.render('pages/widgets/category/category',{
+    };
+    let activeMessage = "";
+    if (req.query.category && req.query.category === "not") {
+        activeMessage = true;
+    }
+    const userName = req.session.fullName;
+    res.render("pages/widgets/category/category", {
         typeInvoice,
         activeMessage,
-        userName
+        userName,
     });
-}
-export const categoryCreate = (req,res)=>{
-    const userName  = req.session.fullName;
-    res.render('pages/widgets/category/category-create',{
-        data:true,
-        categories:true,
-        userName
+};
+export const categoryCreate = (req, res) => {
+    const userName = req.session.fullName;
+    res.render("pages/widgets/category/category-create", {
+        data: true,
+        categories: true,
+        userName,
     });
-}
-export const categoryEdit = async (req,res)=>{
-    const userName  = req.session.fullName;
-    const category =  await Category.findByPk(req.query.id);
-    const select = (type,value)=>{
-        return(type == value ? 'selected' : '');
+};
+export const categoryEdit = async (req, res) => {
+    const userName = req.session.fullName;
+    const category = await Category.findByPk(req.query.id);
+    const select = (type, value) => {
+        return type == value ? "selected" : "";
+    };
+
+    const selIcome = select(category.type, "income");
+    const selExpense = select(category.type, "expense");
+
+    res.render("pages/widgets/category/category-edit", {
+        category,
+        selects: { selIcome, selExpense },
+        userName,
+    });
+};
+export const save = async (req, res) => {
+    if (req.body.action && req.body.action === "create") {
+        const categoryCreate = Category.build({
+            user_id: req.session.user,
+            name: req.body.name,
+            description: req.body.description,
+            type: req.body.type,
+        });
+
+        if (!(await categoryCreate.save())) {
+            res.json({
+                message: "Ooops, algo deu errado, contate o admin",
+                type: "error",
+            });
+            return;
+        }
+
+        res.json({ redirect: "/categoria-editar?id=" + categoryCreate.id });
+        return;
     }
-  
-    const selIcome = select(category.type,'income');
-    const selExpense = select(category.type,'expense');
 
-
-  res.render('pages/widgets/category/category-edit',{
-    category,
-    selects:{selIcome,selExpense},
-    userName
-    
-  });
-}
-export const save = async (req,res)=>{
-
-  if(req.body.action && req.body.action === 'create'){
-   
-    const categoryCreate = Category.build({
-      user_id:req.session.user,
-      name:req.body.name,
-      description:req.body.description,
-      type:req.body.type
-    })
-
-    if(!await categoryCreate.save()){
-      res.json({message:"Ooops, algo deu errado, contate o admin",type:'error'})
-      return
+    if (req.body.action && req.body.action === "update") {
+        const categoryUpdate = Category.update(
+            {
+                name: req.body.name,
+                description: req.body.description,
+                type: req.body.type,
+            },
+            {
+                where: {
+                    id: req.body.category_id,
+                },
+            }
+        );
+        if (!categoryUpdate) {
+            res.json({
+                message: "Ooops, algo deu errado, contate o admin",
+                type: "error",
+            });
+            return;
+        }
+        res.json({ message: "Registro atualizado", type: "success" });
+        return;
     }
 
-    res.json({redirect:"/categoria-editar?id="+ categoryCreate.id})
-    return
-
-  }
-
-  if(req.body.action && req.body.action === 'update'){
-   
-    const categoryUpdate = Category.update({
-      name:req.body.name,
-      description:req.body.description,
-      type:req.body.type
-    },
-    {
-      where:{
-        id:req.body.category_id
-      }
+    if (req.body.action && req.body.action === "delete") {
+        const categoryDelete = Category.destroy({
+            where: {
+                id: req.body.id,
+            },
+        });
+        if (!categoryDelete) {
+            res.json({
+                message: "Ooops, algo deu errado, contate o admin",
+                type: "error",
+            });
+            return;
+        }
+        res.json({ redirect: "/categorias" });
+        return;
     }
-    )
-    if(!categoryUpdate){
-      res.json({message:"Ooops, algo deu errado, contate o admin",type:'error'})
-        return
-    }
-    res.json({message:"Registro atualizado",type:'success'})
-      return
-    
-  }
-
-  if(req.body.action && req.body.action === 'delete'){
-   
-    const categoryDelete = Category.destroy({
-      where:{
-        id:req.body.id
-      }
-    })
-    if(!categoryDelete){
-      res.json({message:"Ooops, algo deu errado, contate o admin",type:'error'})
-    return
-    }
-    res.json({redirect:"/categorias"})
-    return
-    
-  }
-
-
-
-}
-
+};
