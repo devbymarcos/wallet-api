80b7fb7e9e40a100f1ddf8131b8fd28aaf05b70d
Marcos <devbymarcos@gmail.com>
2022-06-06 17:00:18 -0300

    FREATURE :  rotas protegidas

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,23 +5,22 @@ import * as LoginController from "../controllers/loginController.js";
 import * as InvoiceController from "../controllers/InvoiceController.js";
 import * as ExpenseController from "../controllers/expenseController.js";
 import * as IncomeController from "../controllers/incomeController.js";
-
+import * as Auth from "../middlewares/Auth.js";
 const router = Router();
 
 router.post("/login", LoginController.loginAuth);
-
 // DASH
-router.get("/open-invoice", InvoiceController.openInvoice);
-router.post("/chartdata", InvoiceController.dataChart);
-router.post("/panels", InvoiceController.panelsData);
+router.get("/open-invoice", Auth.privateRouter, InvoiceController.openInvoice);
+router.post("/chartdata", Auth.privateRouter, InvoiceController.dataChart);
+router.post("/panels", Auth.privateRouter, InvoiceController.panelsData);
 // CATEGORY
-router.get("/category", CategoryController.category);
+router.get("/category", Auth.privateRouter, CategoryController.category);
 //WALLET
-router.get("/wallet", WalletController.wallet);
+router.get("/wallet", Auth.privateRouter, WalletController.wallet);
 
 //EXPENSE
-router.get("/expense", ExpenseController.expense);
+router.get("/expense", Auth.privateRouter, ExpenseController.expense);
 //INCOME
-router.get("/income", IncomeController.income);
+router.get("/income", Auth.privateRouter, IncomeController.income);
 
 export default router;
