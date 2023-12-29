c42c5a42eec6a0694ef7ed7556dcfeab0278a281
devbymarcos <marcoslopes.dev@gmail.com>
2023-12-29 14:44:27 -0300

    refactor: modificado a ordem das rotas

--- src/routes/router.js
+++ src/routes/router.js
@@ -12,21 +12,25 @@ import * as ExtractController from "../controllers/extractController.js";
 const router = Router();
 router.get("/", (req, res) => res.status(200).json({ message: "activate" }));
 router.post("/login", LoginController.loginAuth);
+//USER
+router.get("/user", Auth.privateRouter, UserController.getUser);
+router.post("/user", UserController.registerUser);
+router.put("/user", Auth.privateRouter, UserController.updateUser);
+router.put("/userpass", Auth.privateRouter, UserController.updatePassword);
 // DASH
 router.get("/dash", Auth.privateRouter, InvoiceController.dashBoard);
-// CATEGORY
-router.get("/categories", Auth.privateRouter, CategoryController.categories);
-router.get("/category/:id", Auth.privateRouter, CategoryController.category);
-router.post("/category", Auth.privateRouter, CategoryController.create);
-router.delete("/category/:id", Auth.privateRouter, CategoryController.remove);
-router.put("/category", Auth.privateRouter, CategoryController.update);
 //WALLET
 router.get("/wallets", Auth.privateRouter, WalletController.wallets);
 router.get("/wallet/:id", Auth.privateRouter, WalletController.walletUniq);
 router.post("/wallet", Auth.privateRouter, WalletController.walletCreate);
 router.put("/wallet", Auth.privateRouter, WalletController.walletUpdate);
 router.delete("/wallet/:id", Auth.privateRouter, WalletController.walletDelete);
-
+// CATEGORY
+router.get("/categories", Auth.privateRouter, CategoryController.categories);
+router.get("/category/:id", Auth.privateRouter, CategoryController.category);
+router.post("/category", Auth.privateRouter, CategoryController.create);
+router.delete("/category/:id", Auth.privateRouter, CategoryController.remove);
+router.put("/category", Auth.privateRouter, CategoryController.update);
 //INVOICE
 router.get("/invoice", Auth.privateRouter, InvoiceController.invoice);
 router.get("/invoice/:id", Auth.privateRouter, InvoiceController.invoiceSingle);
@@ -34,10 +38,6 @@ router.post("/invoice", Auth.privateRouter, InvoiceController.create);
 router.put("/invoice", Auth.privateRouter, InvoiceController.update);
 router.delete("/invoice/:id", Auth.privateRouter, InvoiceController.remove);
 router.post("/transfer", Auth.privateRouter, InvoiceController.transfers);
-//USER
-router.post("/user", UserController.registerUser);
-router.get("/user", Auth.privateRouter, UserController.getUser);
-router.put("/user", Auth.privateRouter, UserController.updateUser);
 
 //TODO CRIAR PARAMETROS PARA ESSA ROTA
 router.get("/extract", Auth.privateRouter, ExtractController.extract);
